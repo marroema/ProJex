fef03b6fe6b382f8af8df1c1c4bc983c0630f3d1
marroema <emanuelbadilla117@gmail.com>
2016-09-06 16:08:47 -0600

    Second Commit

--- config.rb
+++ config.rb
@@ -1,20 +1,21 @@
 require 'bootstrap-sass'
 require 'compass/import-once/activate'
+require 'font-awesome-sass'
 # Require any additional compass plugins here.
 
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "./"
 css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "imagex"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+# output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
